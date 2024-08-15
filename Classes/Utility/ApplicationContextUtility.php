a553e4b9a435545b76cd34b5ff34221a83818a3d
Konrad Michalik <konrad.michalik@xima.de>
2024-08-15 11:58:53 +0200

    feat: use website title for technical context hint

--- Classes/Utility/ApplicationContextUtility.php
+++ Classes/Utility/ApplicationContextUtility.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Xima\XimaTypo3Toolbox\Utility;
 
 use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Site\SiteFinder;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use Xima\XimaTypo3Toolbox\Configuration;
 
 class ApplicationContextUtility
@@ -17,5 +19,13 @@ class ApplicationContextUtility
     public function getContextColor(): string
     {
         return $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS'][Configuration::EXT_KEY]['contextColors'][Environment::getContext()->__toString()] ?? 'transparent';
+    }
+
+    public function getWebsiteTitle(): string
+    {
+        $pid = $GLOBALS['TSFE']->id;
+        $siteFinder = GeneralUtility::makeInstance(SiteFinder::class);
+        $site = $siteFinder->getSiteByPageId($pid);
+        return $site->getConfiguration()['websiteTitle'];
     }
 }
